e50d9c86ed323a54de11ef09ca9780cd57a2d1c8
kienphamlalala <kipham4@gmail.com>
2022-05-27 22:03:33 +0700

    asdasd

--- Group_9/members/models.py
+++ Group_9/members/models.py
@@ -9,25 +9,25 @@ from django.db import models
 
 
 class Dt3(models.Model):
-    #id = models.IntegerField(db_column='ID', blank=True, null = False , primary_key=True)  # Field name made lowercase.
+    id = models.IntegerField(db_column='ID', blank=True, null = False , primary_key=True)  # Field name made lowercase.
     
-    name = models.TextField(db_column='Name', blank=True, null=True)  # Field name made lowercase.
-    brand = models.TextField(db_column='Brand', blank=True, null=True)  # Field name made lowercase.
-    display = models.TextField(db_column='Display', blank=True, null=True)  # Field name made lowercase.
-    hdh = models.TextField(db_column='HDH', blank=True, null=True)  # Field name made lowercase.
-    camera_sau = models.TextField(db_column='Camera_sau', blank=True, null=True)  # Field name made lowercase.
-    camera_truoc = models.TextField(db_column='Camera_truoc', blank=True, null=True)  # Field name made lowercase.
-    chip = models.TextField(db_column='Chip', blank=True, null=True)  # Field name made lowercase.
-    ram = models.TextField(db_column='Ram', blank=True, null=True)  # Field name made lowercase.
-    rom = models.TextField(db_column='Rom', blank=True, null=True)  # Field name made lowercase.
-    sim = models.TextField(db_column='Sim', blank=True, null=True)  # Field name made lowercase.
-    battery = models.TextField(db_column='Battery', blank=True, null=True)  # Field name made lowercase.
-    gia = models.IntegerField(db_column='Gia', blank=True, null=True)  # Field name made lowercase.
-    danhgia = models.FloatField(db_column='Danhgia', blank=True, null=True)  # Field name made lowercase.
-    soluong = models.IntegerField(db_column='SoLuong', blank=True, null=True)  # Field name made lowercase.
-    link = models.TextField(db_column='Link', blank=True, null=True)  # Field name made lowercase.
-    tenhang = models.TextField(db_column='Tenhang', blank=True, null=True)  # Field name made lowercase.
+    name = models.TextField(db_column='name', blank=True, null=True)  # Field name made lowercase.
+    brand = models.TextField(db_column='brand', blank=True, null=True)  # Field name made lowercase.
+    display = models.TextField(db_column='display', blank=True, null=True)  # Field name made lowercase.
+    hdh = models.TextField(db_column='hdh', blank=True, null=True)  # Field name made lowercase.
+    camera_sau = models.TextField(db_column='camera_sau', blank=True, null=True)  # Field name made lowercase.
+    camera_truoc = models.TextField(db_column='camera_truoc', blank=True, null=True)  # Field name made lowercase.
+    chip = models.TextField(db_column='chip', blank=True, null=True)  # Field name made lowercase.
+    ram = models.TextField(db_column='ram', blank=True, null=True)  # Field name made lowercase.
+    rom = models.TextField(db_column='rom', blank=True, null=True)  # Field name made lowercase.
+    sim = models.TextField(db_column='sim', blank=True, null=True)  # Field name made lowercase.
+    battery = models.TextField(db_column='battery', blank=True, null=True)  # Field name made lowercase.
+    gia = models.IntegerField(db_column='price', blank=True, null=True)  # Field name made lowercase.
+    danhgia = models.TextField(db_column='danhgia', blank=True, null=True)  # Field name made lowercase.
+    soluong = models.IntegerField(db_column='sluong', blank=True, null=True)  # Field name made lowercase.
+    link = models.TextField(db_column='link', blank=True, null=True)  # Field name made lowercase.
+    #tenhang = models.TextField(db_column='Tenhang', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
-        db_table = 'dt3'
+        db_table = 'dienthoai1'
