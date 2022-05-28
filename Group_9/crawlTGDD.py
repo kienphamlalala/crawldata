e9d0c9f2d0c45c898222e69db04cdbfe76ea5836
kienphamlalala <kipham4@gmail.com>
2022-05-28 22:07:49 +0700

    assadas

--- Group_9/crawlTGDD.py
+++ Group_9/crawlTGDD.py
@@ -162,8 +162,256 @@ def crawl_cua_tgdd(numPg):
         #driver.close()
     except TypeError:
         c.close() 
-        
-   
-       
+def crawl_Cellphones(numPg):
+    try:
 
+        conn = sqlite3.connect('dienthoai.db')
+        c = conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS dienthoai1(
+                    
+                    name TEXT,
+                    brand TEXT,
+                    display TEXT,
+                    hdh TEXT,
+                    camera_sau TEXT,
+                    camera_truoc TEXT,
+                    chip TEXT,
+                    ram TEXT,
+                    rom TEXT,
+                    sim TEXT,
+                    battery TEXT,
+                
+                    price INTEGER,
+                    danhgia TEXT,
+                    sluong TEXT,
+                    link TEXT
+                    )""")
+        
+        def crawl_cellphones(dienthoai):
+            with conn:
+                id=1
+                c.execute(sql_insert,{'ID':id,'name':dienthoai.name,'brand':dienthoai.brand,'display':dienthoai.display,'hdh':dienthoai.hdh,'camera_sau':dienthoai.camera_sau,'camera_truoc':dienthoai.camera_truoc,'chip':dienthoai.chip,'ram':dienthoai.ram,'rom':dienthoai.rom,'sim':dienthoai.sim,'battery':dienthoai.battery,'price':dienthoai.price,'danhgia':dienthoai.danhgia,'sluong':dienthoai.sluong,'link':dienthoai.link})
+        driver=webdriver.Edge('D:\VISUAL\python\CrawlDataFromWeb\msedgedriver.exe')
+        url='https://cellphones.com.vn/mobile.html' #Lấy link để mở web tự động
+        driver.get(url)
+
+        def GetUrl():
+            page_s=BeautifulSoup(driver.page_source,"html.parser") #dùng bs4 tải toàn bộ HTML document lên chương trình
+            tel_infors=page_s.find_all('div',class_='block-products san-pham-cate')
+            div1=tel_infors[0].find_all('div',class_='box-products')
+            div2=div1[0].find_all('div',class_='item-product')
+            all_prod=[]
+
+            for tel_infor in div2:
+                tel_ID=tel_infor.find('a').get('href')
+            
+                if tel_ID!=None and tel_ID not in all_prod:
+                    all_prod.append(tel_ID)
+                set(all_prod)
+
+            return all_prod
+
+
+        
+        Url_all=[]
+        for i in range(numPg):
+            #sroll down until found element next button 
+            driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_xpath("/html/body/div[1]/div/section/div/div[5]/div/a"))
+            driver.execute_script("window.scrollBy(0, -150);")
+            e=driver.find_element_by_xpath("/html/body/div[1]/div/section/div/div[5]/div/a")
+            e.click()
+        time.sleep(13)
+
+        Url_all=[]
+        Url_all =GetUrl()
+
+        for url in Url_all:
+
+                driver.get(url)
+                page_source=BeautifulSoup(driver.page_source,"html.parser")
+                div1=page_source.find_all('table',id='tskt')
+                #get text of element has tag td in div1
+                try:
+                        display=div1[0].find_all('th')[23].get_text()
+                except IndexError:
+                        continue
+                name=page_source.find_all('div',class_='box-name__box-product-name')[0].get_text()
+                camera_sau=display=div1[0].find_all('th')[5].get_text()
+                camera_truoc=display=div1[0].find_all('th')[7].get_text()
+                chip=display=div1[0].find_all('th')[9].get_text()
+                ram=display=div1[0].find_all('th')[11].get_text()
+                rom=display=div1[0].find_all('th')[13].get_text()
+                battery=display=div1[0].find_all('th')[15].get_text()
+                sim=display=div1[0].find_all('th')[17].get_text()
+                hdh=display=div1[0].find_all('th')[19].get_text()
+                sl_tmp=page_source.find_all('p',class_='rating-total')
+                soluong=sl_tmp[0].get_text()
+                soluong=soluong.split(' ')
+                soluong=int(soluong[0])
+                dgtmp=page_source.find_all('div',class_='chart-vote__box-left')[0].get_text()
+                danhgia=dgtmp.split('/')
+                danhgia=float(danhgia[0])
+                
+                nameshop='Cellphones'
+                link=url
+                brand=name.split(' ')[0]
+                try:
+                        giatmp=page_source.find_all('p',class_='special-price')[0].get_text()
+                        gia=giatmp.replace('₫','')
+                        gia=int(gia.replace('.',''))
+                except AttributeError:
+                        giatmp=page_source.find_all('p',class_='old-price')[0].get_text()
+                        gia=giatmp.replace('₫','')
+                        gia=int(gia.replace('.',''))
+                        
+                except IndexError:
+                        continue
+                brand=brand.strip()
+                name=name.strip()
+                print(name,brand,display,hdh,camera_sau,camera_truoc,chip,ram,rom,sim,battery,gia,danhgia,soluong,link)
+                sql_insert="INSERT INTO dienthoai1 VALUES(:ID,:name,:brand,:display,:hdh,:camera_sau,:camera_truoc,:chip,:ram,:rom,:sim,:battery,:price,:danhgia,:sluong,:link)"
+                dt1= dienthoai (name,brand,display,hdh,camera_sau,camera_truoc,chip,ram,rom,sim,battery,gia,danhgia,soluong,link)
+
+                crawl_cellphones(dt1)
+    
+    except IndexError:
+        c.close()
+        #driver.close()
+    except TypeError:
+        c.close()     
+   
+def Crawl_cua_FPT(numPg):
+    try:
+        conn = sqlite3.connect('dienthoai.db')
+        c = conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS dienthoai3(
+                    
+                    name TEXT,
+                    brand TEXT,
+                    display TEXT,
+                    hdh TEXT,
+                    camera_sau TEXT,
+                    camera_truoc TEXT,
+                    chip TEXT,
+                    ram TEXT,
+                    rom TEXT,
+                    sim TEXT,
+                    battery TEXT,
+                
+                    price INTEGER,
+                    danhgia TEXT,
+                    sluong TEXT,
+                    link TEXT
+                    )""")
+        #create class dienthoai
+        #delete all data in table dienthoai2.db
+        #c.execute("DELETE FROM dienthoai3")
+        def crawl_tgdd(dienthoai):
+            with conn:
+                id=1
+                c.execute(sql_insert,{'ID':id,'name':dienthoai.name,'brand':dienthoai.brand,'display':dienthoai.display,'hdh':dienthoai.hdh,'camera_sau':dienthoai.camera_sau,'camera_truoc':dienthoai.camera_truoc,'chip':dienthoai.chip,'ram':dienthoai.ram,'rom':dienthoai.rom,'sim':dienthoai.sim,'battery':dienthoai.battery,'price':dienthoai.price,'danhgia':dienthoai.danhgia,'sluong':dienthoai.sluong,'link':dienthoai.link})
+        driver=webdriver.Edge('D:\VISUAL\python\CrawlDataFromWeb\msedgedriver.exe')
+        url='https://fptshop.com.vn/dien-thoai' #Lấy link để mở web tự động
+        driver.get(url)
+        def GetUrl():
+            page_s=BeautifulSoup(driver.page_source,"html.parser") #dùng bs4 tải toàn bộ HTML document lên chương trình
+
+            tel_infors=page_s.find('div',class_='cdt-product-wrapper m-b-20')
+
+            all_prod=[]
+
+            for tel_infor in tel_infors:
+                
+                tel_ID=tel_infor.find('a').get('href') # lấy link đuôi trong thành phần có tên là href
+                tel_If_url=''
+                if tel_ID !=None and tel_If_url not in all_prod:
+                    tel_If_url='https://fptshop.com.vn'+tel_ID
+                    all_prod.append(tel_If_url)
+                set(all_prod)
+            return all_prod
+            
+
+         
+        Url_all=[]
+        
+        for i in range(numPg):
+            driver.execute_script('window.scrollTo(0,document.body.scrollHeight)') # thực hiện kéo xuống xuối cuối trang 
+            next_btn=driver.find_element_by_xpath('//*[@id="root"]/main/div/div[3]/div[2]/div[2]/div/div[3]/a')
+            next_btn.click()
+        driver.execute_script('window.scrollTo(0,document.body.scrollHeight)') # thực hiện kéo xuống xuối cuối trang 
+        Url_each_page=GetUrl() # Lấy thông tin tất cả từng trang
+        Url_all=Url_all+Url_each_page 
+
+        for url in Url_all:
+
+            driver.get(url)
+            page_source=BeautifulSoup(driver.page_source,"html.parser")
+
+            div1=page_source.find('table',class_='st-pd-table')
+
+            try :
+                display=div1.find_all('td')[1].get_text()
+            except AttributeError                            :
+                continue
+
+            name=page_source.find_all('h1',class_='st-name')[0].get_text()
+            name=name.split(' ')
+            brand=name[0]
+            name2=name.pop(-1)
+            name=" ".join(name)
+            ram=div1.find_all('td')[7].get_text()
+            rom=div1.find_all('td')[9].get_text()
+            cpu=div1.find_all('td')[11].get_text()
+            try:
+                battery=div1.find_all('td')[15].get_text()
+            except IndexError:
+                continue
+            try:
+                hdh=div1.find_all('td')[19].get_text()
+            except IndexError:
+                continue
+
+            sim=div1.find_all('td')[17].get_text()
+
+
+            camera_sau=div1.find_all('td')[3].get_text()
+            camera_truoc=div1.find_all('td')[5].get_text()
+
+
+            nameshop="Cửa hàng FPT"
+
+
+            gia_tmp=page_source.find_all('div',class_='st-price-main')[0].get_text()
+            gia=gia_tmp.replace('₫','')
+            gia=int(gia.replace('.',''))
+
+            try :
+
+                danhgia=page_source.find_all('div',class_='point')[0].get_text()
+                danhgia=danhgia.split('/')
+                danhgia=float(danhgia[0])
+            except IndexError :
+                continue
+
+
+            soluong=page_source.find_all('a',class_='re-link',id='re-rate')[0].get_text()
+            soluong=soluong.split(' ')
+            soluong=int(soluong[0])
+
+            link=url
+            #INSERT ALL DATA TO DATABASE
+            print(name,brand,display,hdh,camera_sau,camera_truoc,cpu,ram,rom,sim,battery,gia,danhgia,soluong,link)
+            sql_insert="INSERT INTO dienthoai3 VALUES(:name,:brand,:display,:hdh,:camera_sau,:camera_truoc,:chip,:ram,:rom,:sim,:battery,:price,:danhgia,:sluong,:link)"
+            dt1= dienthoai (name,brand,display,hdh,camera_sau,camera_truoc,cpu,ram,rom,sim,battery,gia,danhgia,soluong,link)
+            
+            crawl_tgdd(dt1)
+            
+        
+        c.close() 
+
+    except IndexError:
+        c.close()
+        #driver.close()
+    except TypeError:
+        c.close()     
 
--- Group_9/members/views.py
+++ Group_9/members/views.py
@@ -15,8 +15,18 @@ def index(request):
 
 def crawl_TGDD(request):
     # get the number of pages from html page index.html
-    
-    numPg1=request.GET['page']
-    numPg=int(numPg1)
-    b=crawlTGDD.crawl_cua_tgdd(numPg)
+    link_name=request.GET['linkname']
+    if link_name=='TGDD':
+      
+      numPg1=request.GET['page']
+      numPg=int(numPg1)
+      b=crawlTGDD.crawl_cua_tgdd(numPg)
+    elif link_name=='FPT':
+      numPg1=request.GET['page']
+      numPg=int(numPg1)
+      b=crawlTGDD.Crawl_cua_FPT(numPg)
+    elif link_name=='cellphones' :
+      numPg1=request.GET['page']
+      numPg=int(numPg1)
+      b=crawlTGDD.crawl_Cellphones(numPg)
     return HttpResponse("""<html><script>window.location.replace('/');</script></html>""")
